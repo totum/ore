c450b8a715221d0f7914c9d3fefe2fa4187ce69a
totum <vallebo@gmail.com>
2014-05-27 00:25:34 -0700

    naming convention corrections

--- ore_t-head.php
+++ ore_t-head.php
@@ -15,15 +15,4 @@
 	<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame -->
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	
-
-	<!-- IE stuff -->
-	<? /*
-	Target IE browsers specifically. Uncomment and place in own partial if needed.
-		<!--[if lt IE 7 ]> <body class="ie6"> <![endif]-->
-		<!--[if IE 7 ]>    <body class="ie7"> <![endif]-->
-		<!--[if IE 8 ]>    <body class="ie8"> <![endif]-->
-		<!--[if IE 9 ]>    <body class="ie9"> <![endif<]-->
-		<!--[if (gt IE 9)|!(IE)]><!-->  <!--<![endif]-->
-	*/ ?>
-
 	<?php wp_head(); ?>
